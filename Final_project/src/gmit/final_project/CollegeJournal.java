ca0ad01f8741307f0420ececcfd8e48fadb50639
Conor <Conor@192.168.1.25>
2018-12-03 21:53:46 +0000

    added create note function

--- Final_project/src/gmit/final_project/CollegeJournal.java
+++ Final_project/src/gmit/final_project/CollegeJournal.java
@@ -20,15 +20,10 @@ public class CollegeJournal
 		createModule("Business", "BS");
 		createModule("Tech Drawing", "TD");
 
-		for(int i=0;i<modules.size();i++)
+		/*for(int i=0;i<modules.size();i++)
 		{
 			System.out.println(modules.get(i).getModuleName() + "\t" + modules.get(i).getModuleShortName());
-		}
-		//System.out.println(m1.getModuleName());
-		//System.out.println(m2.getModuleName());
-
-		//System.out.println(m3.getModuleShortName());
-		//System.out.println(m4.getModuleShortName());
+		}*/
 
 		Note n1 = new Note("Programming","Lecture 1");
 		Note n2 = new Note("Testing","Lab 1");
@@ -39,8 +34,15 @@ public class CollegeJournal
 		n3.setNoteContents("This is an example note to see if this works");
 		n4.setNoteContents("Another example note to see if this works");
 
-		System.out.println(n3.fullNote());
-		System.out.println(n4.fullNote());
+		//System.out.println(n3.fullNote());
+		//System.out.println(n4.fullNote());
+
+		createNote("Programming", "Lecture 2000", "Today i learned about inner loops and FX");
+		createNote("Testing", "XXXXXXXXXX", "Going to test this note app");
+
+		for(int i=0;i<notes.size();i++){
+			System.out.println(notes.get(i).fullNote() +"\n");
+		}
 	}
 	public static void createModule(String mn, String sn)
 	{
@@ -55,10 +57,24 @@ public class CollegeJournal
 		if(!exists){
 			Module temp = new Module(mn, sn);
 			modules.add(temp);
-			System.out.println("Module Successfully Added to ArrayList");
+			//System.out.println("Module Successfully Added to ArrayList");
 		} else {
 			System.out.println("Not Created");
 		}
+	}
+	public static void createNote(String mn, String nn, String c)
+	{
+		for(int i=0;i<notes.size();i++)
+		{
+			if(mn == notes.get(i).getModuleName() && nn == notes.get(i).getNoteName())
+			{
+				System.out.println("Note with the same title already exists in the module\n-Copy added to name for distinction");
+				nn+=" -Copy";
 
+			}
+		}
+		Note temp = new Note(mn, nn, c);
+		notes.add(temp);
+		System.out.println("Note " + nn + " added successfully");
 	}
 }
